b965efd819c2f54952f1ad8f9a21b5b928045b66
Markus Grohs <markus.grohs@aixigo.de>
2014-10-23 18:23:30 +0200

    OSS-74: Minor changes

--- includes/widgets/chart_demo/table_editor_widget/table_editor_widget.js
+++ includes/widgets/chart_demo/table_editor_widget/table_editor_widget.js
@@ -32,18 +32,14 @@ define([
         };
 
         $scope.createRandomData = function() {
-            $scope.data = createRandomMatrix(3, 5, 1, 10);
+            $scope.matrix = createRandomMatrix(3, 5, 1, 10);
             //settingFactory.renderHandsontable($('#my-table'));
-            settingFactory.updateHandsontableSettings($('#my-table'), {data: $scope.data});
+            settingFactory.updateHandsontableSettings($('#my-table'), {data: $scope.matrix});
 
-        };
-
-        $scope.modify = function () {
-            $scope.data[0][0] = 100;
         };
 
         $scope.output = function () {
-            console.log($scope.data);
+            console.log($scope.matrix);
         }
 
 
@@ -52,7 +48,17 @@ define([
             for (var i = 0; i < rows; ++i) {
                 array[i] = new Array(columns)
             }
-            //array[1][1] = "Hello World!"; //FIXME CSS: Provoke a styling error due to word wrapping.
+            return array;
+        }
+
+        var createBuggyStylingMatrix = function(rows, columns) {
+            var array = new Array(rows);
+            for (var i = 0; i < rows; ++i) {
+                array[i] = new Array(columns)
+                for (var j=0; j<columns; ++j) {
+                    array[i][j] = "Hello World 123!";
+                }
+            }
             return array;
         }
 
@@ -68,119 +74,90 @@ define([
         }
 
         // Initialize an empty 2-dimensional array of fixed size.
-        $scope.data = createEmptyMatrix(3, 5);
+        $scope.matrix = createEmptyMatrix(3, 5);
+        //$scope.matrix = createBuggyStylingMatrix(3, 5); //FIXME CSS: Provoke a styling error due to word wrapping.
 
-
-
-
-
-
-
-
-
-
-        //$scope.colHeaders = ['X1', 'X2'];
-        //$scope.rowHeaders = ['Y1','Y2','Y3'];
-//       $scope.rowHeaders = function(i) {
-//           console.log(i);
-//           return 'Y' + i;
-//       }
-//
-//        $scope.colHeaders = function (j) {
-//            console.log("col: X" + j);
-//            return 'X' + j;
-//        }
-//
-//        $scope.foo = function () {
-//            return 'dfd';
-//        }
-//
-//        $scope.db = {};
-//
-//        $scope.db.rowheaders = ['A', 2, 3];
-//
-//        $scope.db.items = [
-//            {
-//                "id": 1,
-//                "name": {
-//                    "first": "John",
-//                    "last": "Schmidt"
-//                },
-//                "address": "45024 France",
-//                "price": 760.41,
-//                "isActive": "Yes",
-//                "product": {
-//                    "description": "Fried Potatoes",
-//                    "options": [
-//                        {
-//                            "description": "Fried Potatoes",
-//                            "image": "//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png",
-//                            "Pick$": null
-//                        },
-//                        {
-//                            "description": "Fried Onions",
-//                            "image": "//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png",
-//                            "Pick$": null
-//                        }
-//                    ]
-//                }
-//            },
-//            {
-//                "id": 2,
-//                "name": {
-//                    "first": "John",
-//                    "last": "Schmidt"
-//                },
-//                "address": "45024 France",
-//                "price": 760.41,
-//                "isActive": "Yes",
-//                "product": {
-//                    "description": "Fried Potatoes",
-//                    "options": [
-//                        {
-//                            "description": "Fried Potatoes",
-//                            "image": "//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png",
-//                            "Pick$": null
-//                        },
-//                        {
-//                            "description": "Fried Onions",
-//                            "image": "//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png",
-//                            "Pick$": null
-//                        }
-//                    ]
-//                }
-//            },
-//            //more items go here
-//            {
-//                "id": 3,
-//                "name": {
-//                    "first": "John",
-//                    "last": "Schmidt"
-//                },
-//                "address": "45024 France",
-//                "price": 760.41,
-//                "isActive": "Yes",
-//                "product": {
-//                    "description": "Fried Potatoes",
-//                    "options": [
-//                        {
-//                            "description": "Fried Potatoes",
-//                            "image": "//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png",
-//                            "Pick$": null
-//                        },
-//                        {
-//                            "description": "Fried Onions",
-//                            "image": "//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png",
-//                            "Pick$": null
-//                        }
-//                    ]
-//                }
-//            }
-//            //more items go here
-//            //more items go here
-//        ];
-
-
+        // Just for demo purposes. Will result in styling errors due to word wrapping and/or manually settings column widths.
+        /*
+        $scope.db={};
+        $scope.db.items = [
+            {
+                "id":1,
+                "name":{
+                    "first":"John",
+                    "last":"Schmidt"
+                },
+                "address":"45024 France",
+                "price":760.41,
+                "isActive":"Yes",
+                "product":{
+                    "description":"Fried Potatoes",
+                    "options":[
+                        {
+                            "description":"Fried Potatoes",
+                            "image":"//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png",
+                            "Pick$":null
+                        },
+                        {
+                            "description":"Fried Onions",
+                            "image":"//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png",
+                            "Pick$":null
+                        }
+                    ]
+                }
+            },
+            {
+                "id":2,
+                "name":{
+                    "first":"John",
+                    "last":"Schmidt"
+                },
+                "address":"45024 France",
+                "price":760.41,
+                "isActive":"Yes",
+                "product":{
+                    "description":"Fried Potatoes",
+                    "options":[
+                        {
+                            "description":"Fried Potatoes",
+                            "image":"//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png",
+                            "Pick$":null
+                        },
+                        {
+                            "description":"Fried Onions",
+                            "image":"//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png",
+                            "Pick$":null
+                        }
+                    ]
+                }
+            },
+            {
+                "id":3,
+                "name":{
+                    "first":"John",
+                    "last":"Schmidt"
+                },
+                "address":"45024 France",
+                "price":760.41,
+                "isActive":"Yes",
+                "product":{
+                    "description":"Fried Potatoes",
+                    "options":[
+                        {
+                            "description":"Fried Potatoes",
+                            "image":"//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png",
+                            "Pick$":null
+                        },
+                        {
+                            "description":"Fried Onions",
+                            "image":"//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png",
+                            "Pick$":null
+                        }
+                    ]
+                }
+            }
+        ];
+        */
     }
 
     module.controller(moduleName + '.Controller', Controller);
