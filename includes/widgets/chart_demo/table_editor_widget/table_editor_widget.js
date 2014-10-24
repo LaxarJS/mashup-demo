374b26d8982086990b499c6cdf3f51bf33b2b1d9
Jonas Schulte <jonas.schulte@aixigo.de>
2014-10-24 13:01:17 +0200

    OSS-74: added scrollbar to TableEditorWidget

--- includes/widgets/chart_demo/table_editor_widget/table_editor_widget.js
+++ includes/widgets/chart_demo/table_editor_widget/table_editor_widget.js
@@ -18,14 +18,30 @@ define( [
 
    function Controller( $scope, settingFactory, autoCompleteFactory ) {
 
+
+      // Initialize an empty 2-dimensional array of fixed size.
+      //$scope.matrix = createEmptyMatrix( 3, 5 );
+      $scope.matrix = createBuggyStylingMatrix(3, 6); //FIXME CSS: Provoke a styling error due to word wrapping.
+
       // Check if the injection worked.
       //console.log(settingFactory);
       //console.log(autoCompleteFactory);
+
+
+      ng.element( '#my-table' ).ready(function() {
+         //console.log('ready()');
+         //settingFactory.renderHandsontable( ng.element( '#my-table' ) );
+         //settingFactory.updateHandsontableSettings( ng.element( '#my-table' ), { data: $scope.matrix, type: 'numeric', format: '0.0[000]' } );
+      });
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
       // Just a hack to visually "repair" the table by redrawing it.
       $scope.refresh = function () {
          settingFactory.renderHandsontable( ng.element( '#my-table' ) );
       };
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
       $scope.afterChange = function () {
          console.log( 'afterChange()' );
@@ -40,32 +56,43 @@ define( [
          );
       };
 
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
       $scope.createRandomData = function () {
          $scope.matrix = createRandomMatrix( 3, 5, 1, 10 );
          //settingFactory.renderHandsontable($('#my-table'));
-         settingFactory.updateHandsontableSettings( ng.element( '#my-table' ), { data: $scope.matrix, type: 'numeric', format: '0.0[000]' } );
+         settingFactory.updateHandsontableSettings( ng.element( '#my-table' ),
+            {
+               data: $scope.matrix,
+               type: 'numeric',
+               format: '0.0[000]'
+            }
+         );
 
       };
 
-      ng.element( '#my-table' ).ready(function() {
-         //console.log('ready()');
-         //settingFactory.renderHandsontable( ng.element( '#my-table' ) );
-         //settingFactory.updateHandsontableSettings( ng.element( '#my-table' ), { data: $scope.matrix, type: 'numeric', format: '0.0[000]' } );
-      });
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
       $scope.output = function () {
          console.log( $scope.matrix );
       };
 
-      var createEmptyMatrix = function ( rows, columns ) {
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+      function createEmptyMatrix( rows, columns ) {
          var array = new Array( rows );
          for ( var i = 0; i < rows; ++i ) {
             array[ i ] = new Array( columns );
          }
          return array;
-      };
+      }
 
-      var createBuggyStylingMatrix = function ( rows, columns ) {
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      function createBuggyStylingMatrix( rows, columns ) {
          var array = new Array( rows );
          for ( var i = 0; i < rows; ++i ) {
             array[ i ] = new Array( columns );
@@ -74,9 +101,11 @@ define( [
             }
          }
          return array;
-      };
+      }
 
-      var createRandomMatrix = function ( rows, columns, min, max ) {
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      function createRandomMatrix( rows, columns, min, max ) {
          var array = new Array( rows );
          for ( var i = 0; i < rows; ++i ) {
             array[ i ] = new Array( columns );
@@ -85,11 +114,8 @@ define( [
             }
          }
          return array;
-      };
+      }
 
-      // Initialize an empty 2-dimensional array of fixed size.
-      $scope.matrix = createEmptyMatrix( 3, 5 );
-      //$scope.matrix = createBuggyStylingMatrix(3, 5); //FIXME CSS: Provoke a styling error due to word wrapping.
    }
 
    module.controller( moduleName + '.Controller', Controller );
