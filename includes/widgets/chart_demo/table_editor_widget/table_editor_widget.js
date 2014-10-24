492f137555157211e501a5cec2c6cabf7f7465c6
Markus Grohs <markus.grohs@aixigo.de>
2014-10-24 14:42:27 +0200

    OSS-74: Added axVisibilityService directive

--- includes/widgets/chart_demo/table_editor_widget/table_editor_widget.js
+++ includes/widgets/chart_demo/table_editor_widget/table_editor_widget.js
@@ -18,6 +18,90 @@ define( [
 
    function Controller( $scope, settingFactory, autoCompleteFactory ) {
 
+//      $scope.model.settings = {
+//         {colHeaders: colHeaders, contextMenu: ['row_above', 'row_below', 'remove_row'], afterChange: afterChange }
+//      };
+
+
+      $scope.db = {};
+      $scope.db.items = [
+         {
+            "id":1,
+            "name":{
+               "first":"John",
+               "last":"Schmidt"
+            },
+            "address":"45024 France",
+            "price":760.41,
+            "isActive":"Yes",
+            "product":{
+               "description":"Fried Potatoes",
+               "options":[
+                  {
+                     "description":"Fried Potatoes",
+                     "image":"//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png",
+                     "Pick$":null
+                  },
+                  {
+                     "description":"Fried Onions",
+                     "image":"//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png",
+                     "Pick$":null
+                  }
+               ]
+            }
+         },
+         {
+            "id":2,
+            "name":{
+               "first":"John",
+               "last":"Schmidt"
+            },
+            "address":"45024 France",
+            "price":760.41,
+            "isActive":"Yes",
+            "product":{
+               "description":"Fried Potatoes",
+               "options":[
+                  {
+                     "description":"Fried Potatoes",
+                     "image":"//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png",
+                     "Pick$":null
+                  },
+                  {
+                     "description":"Fried Onions",
+                     "image":"//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png",
+                     "Pick$":null
+                  }
+               ]
+            }
+         },
+         {
+            "id":3,
+            "name":{
+               "first":"John",
+               "last":"Schmidt"
+            },
+            "address":"45024 France",
+            "price":760.41,
+            "isActive":"Yes",
+            "product":{
+               "description":"Fried Potatoes",
+               "options":[
+                  {
+                     "description":"Fried Potatoes",
+                     "image":"//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png",
+                     "Pick$":null
+                  },
+                  {
+                     "description":"Fried Onions",
+                     "image":"//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png",
+                     "Pick$":null
+                  }
+               ]
+            }
+         }
+         //more items go here
+      ];
 
       // Initialize an empty 2-dimensional array of fixed size.
       //$scope.matrix = createEmptyMatrix( 3, 5 );
@@ -27,18 +111,17 @@ define( [
       //console.log(settingFactory);
       //console.log(autoCompleteFactory);
 
+      $scope.eventBus.subscribe( 'beginLifecycleRequest', function() {
+            settingFactory.renderHandsontable( ng.element( '#my-table' ) );
+            //settingFactory.updateHandsontableSettings( ng.element( '#my-table' ), { data: $scope.matrix, type: 'numeric', format: '0.0[000]' } );
 
-      ng.element( '#my-table' ).ready(function() {
-         //console.log('ready()');
-         //settingFactory.renderHandsontable( ng.element( '#my-table' ) );
-         //settingFactory.updateHandsontableSettings( ng.element( '#my-table' ), { data: $scope.matrix, type: 'numeric', format: '0.0[000]' } );
-      });
+      } );
 
       ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
       // Just a hack to visually "repair" the table by redrawing it.
       $scope.refresh = function () {
-         settingFactory.renderHandsontable( ng.element( '#my-table' ) );
+         settingFactory.renderHandsontable( ng.element( '#my-table2' ) );
       };
 
       ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -58,18 +141,18 @@ define( [
 
       ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-      $scope.createRandomData = function () {
-         $scope.matrix = createRandomMatrix( 3, 5, 1, 10 );
-         //settingFactory.renderHandsontable($('#my-table'));
-         settingFactory.updateHandsontableSettings( ng.element( '#my-table' ),
-            {
-               data: $scope.matrix,
-               type: 'numeric',
-               format: '0.0[000]'
-            }
-         );
-
-      };
+//      $scope.createRandomData = function () {
+//         $scope.matrix = createRandomMatrix( 3, 5, 1, 10 );
+//         //settingFactory.renderHandsontable($('#my-table'));
+//         settingFactory.updateHandsontableSettings( ng.element( '#my-table' ),
+//            {
+//               data: $scope.matrix,
+//               type: 'numeric',
+//               format: '0.0[000]'
+//            }
+//         );
+//
+//      };
 
       ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -121,6 +204,22 @@ define( [
    module.controller( moduleName + '.Controller', Controller );
 
    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+   module.directive( 'axTableEditorIfVisible', [ 'axVisibilityService', 'settingFactory', '$timeout', function( visibilityService, settingFactory, $timeout ) {
+      return {
+         link: function( $scope, $element ) {
+            var handler = visibilityService.handlerFor( $scope );
+            handler.onShow( function() {
+               $timeout( function() {
+                  $scope.isVisible = true;
+               }, 0 );
+            } );
+         }
+      }
+   } ]
+   );
+
 
    return module;
 
