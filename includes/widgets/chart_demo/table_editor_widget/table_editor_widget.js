eb9a729bea3b3ec9406c13c77fce9ff9cb8cee90
Jonas Schulte <jonas.schulte@aixigo.de>
2014-10-29 11:30:58 +0100

    OSS-74: fixed spec_data.json and function convertToTableModel

--- includes/widgets/chart_demo/table_editor_widget/table_editor_widget.js
+++ includes/widgets/chart_demo/table_editor_widget/table_editor_widget.js
@@ -26,6 +26,29 @@ define( [
       function convertToTableModel() {
          $scope.model.tableModel = [];
 
+
+         // Column headers
+//         var colHeaders = [];
+//         colHeaders.push( null );
+//         var spreadsheet = $scope.resources.spreadsheet;
+//         spreadsheet.series.forEach( function( value, key ) {
+//            colHeaders.push( value.label );
+//         } );
+//         $scope.model.tableModel.push( colHeaders );
+//
+//
+//
+//         // Data area
+//         spreadsheet.grid.forEach( function( rowHeader, row ) {
+//            var rowData = [];
+//            rowData.push( rowHeader );
+//            $scope.model.tableModel.push( rowData );
+//            spreadsheet.series.forEach( function( value, col ) {
+//               $scope.model.tableModel[ row + 1 ].push( value.data[ row ] );
+//            } );
+//         } );
+
+
          // Column headers
          var colHeaders = [];
          colHeaders.push( null );
@@ -42,40 +65,12 @@ define( [
             $scope.model.tableModel.push( rowData );
          } );
 
-//         // Data area
-//         spreadsheet.grid.forEach( function( rowHeader, row ) {
-//            console.log( $scope.model.tableModel[ row + 1 ] );
-//            spreadsheet.series.data.forEach( function( value, col ) {
-//               //console.log( $scope.model.tableModel[ key + 1 ] );
-//               $scope.model.tableModel[ row + 1 + col ].concat( value );
-//
-//            } );
-//         } );
-
-//         spreadsheet.grid.forEach( function( rowHeader, row ) {
-//            var rowData = [];
-//            rowData.push( rowHeader );
-//            console.log( rowData );
-//            spreadsheet.series.forEach( function( value, col ) {
-//               //console.log( $scope.model.tableModel[ key + 1 ] );
-//               $scope.model.tableModel[ row + 1 + col ].push( value );
-//
-//            } );
-//         } );
-
-
-
-         ////////////////////
-
-//
-//         for (var i=0; i<spreadsheet.series.length+1; ++i) {
-//            for
-//         }
-//
-
-         console.log( colHeaders );
-         console.log( $scope.model.tableModel );
-
+         // Data area
+         spreadsheet.grid.forEach( function( rowHeader, row ) {
+            spreadsheet.series.forEach( function( value, col ) {
+               $scope.model.tableModel[ row + 1 ].push( value.data[ row ] );
+            } );
+         } );
       }
    }
 
