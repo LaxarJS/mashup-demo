7412f9ee342de8adb7a569164125cf6b77c6c458
Jonas Schulte <jonas.schulte@aixigo.de>
2015-12-23 14:33:03 +0100

    (#17) using laxar-mocks instead of laxar-testing now

--- includes/widgets/mashup-demo/data-provider-widget/spec/spec_runner.js
+++ includes/widgets/mashup-demo/data-provider-widget/spec/spec_runner.js
@@ -1,14 +1,17 @@
 /**
  * Copyright 2015 aixigo AG
  * Released under the MIT license.
- * www.laxarjs.org
+ * http://www.laxarjs.org
  */
 ( function( global ) {
    'use strict';
    global.laxarSpec = {
-      title: 'DataProviderWidget Specification',
+      title: 'data-provider-widget Specification',
       tests: [
          'data-provider-widget.spec'
-      ]
+      ],
+      testRunner: 'laxar-mocks',
+      jasmineMajorVersion: 2
    };
 } )( this );
+
